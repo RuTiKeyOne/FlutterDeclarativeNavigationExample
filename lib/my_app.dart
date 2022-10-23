1299943cdfbaaeb079cb063f4268f6d9f5c25c80
rutikeyone <rutikeyone@gmail.com>
2022-10-23 16:36:25 +0700

    add nested router navigation from medium example

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,24 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:flutter_declarative_navigation_example/app/routes/navigation/app_navigation.dart';
-import 'package:flutter_declarative_navigation_example/generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'package:flutter_declarative_navigation_example/app/routes/navigation/app_navigation.dart';
+import 'package:flutter_declarative_navigation_example/generated/l10n.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      home: const AppNavigation(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(),
+//       debugShowCheckedModeBanner: false,
+//       localizationsDelegates: const [
+//         S.delegate,
+//         GlobalMaterialLocalizations.delegate,
+//         GlobalWidgetsLocalizations.delegate,
+//         GlobalCupertinoLocalizations.delegate,
+//       ],
+//       supportedLocales: S.delegate.supportedLocales,
+//       home: const AppNavigation(),
+//     );
+//   }
+// }
